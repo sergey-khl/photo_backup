d049df7b96c9ddabc4bf61256b0cb3f2d80e941e
Sergey Khlynovskiy <sergeykhlynovskiy@gmail.com>
2024-08-02 17:54:52 -0600

    existing remote

--- backup.py
+++ backup.py
@@ -2,7 +2,7 @@ import subprocess
 
 if __name__ == "__main__":
     try:
-        result = subprocess.run(['rclone', 'ls', 'remote:'], check=True, capture_output=True, text=True)
+        result = subprocess.run(['rclone', 'ls', 'mega_bear:'], check=True, capture_output=True, text=True)
         print(result.stdout)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred during the rclone query: {e}")
