66aaa5ce855538dc1dbc99240caab69eda3c48ed
Sergey Khlynovskiy <sergeykhlynovskiy@gmail.com>
2024-08-02 18:13:53 -0600

    try dis

--- backup.py
+++ backup.py
@@ -2,7 +2,9 @@ import subprocess
 
 if __name__ == "__main__":
     try:
-        result = subprocess.run(['rclone', 'ls', 'mega_bear:'], check=True, capture_output=True, text=True)
-        print(result.stdout)
+        DEST="crypt_photos:mega_bear"
+
+        command = f'rclone sync --fast-list --exclude ".thumbnails/**" -P ~/storage/dcim {DEST}'
+        subprocess.run(command.split(' '), check=True, capture_output=True, text=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred during the rclone query: {e}")
